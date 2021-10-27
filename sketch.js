bbe8c3a8878f90c3194f1de2efad8cd4252da27f
Nom-Nom7 <76200202+Nom-Nom7@users.noreply.github.com>
2021-10-27 20:50:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+var player, target;
+
+function preload(){
+
+}
+
+function setup() {
+  createCanvas(1200,800);
+  
+
+
+  target = createSprite(Math.round(random(200,1000)),Math.round(random(200,600)),50,50); 
+  
+  
+}
+
+function draw() {
+  background("cyan"); 
+
+  if(mouseX - target.x <= 75 && mouseX - target.x >= 0
+    && mouseY - target.y <= 25 && mouseY - target.y >= -25) {
+    target.velocityX = Math.round(random(-10,0))
+  }
+
+  if(mouseX - target.x >= -75 && mouseX - target.x <= 0
+    && mouseY - target.y <= 25 && mouseY- target.y >= -25) {
+    target.velocityX = Math.round(random(0,10))
+  }
+
+  if(mouseY - target.y <= 75 && mouseY - target.y >= 0
+    && mouseX - target.x <= 25 && mouseX - target.x >= -25) {
+    target.velocityY = Math.round(random(-10,0))
+  }
+
+  if(mouseY - target.y >= -75 && mouseY - target.y <= 0
+    && mouseX - target.x <= 25 && mouseX - target.x >= -25) {
+    target.velocityY = Math.round(random(0,10))
+  }
+
+   if(mouseY - target.y <= 50 && mouseY - target.y >= 25
+    && mouseX - target.x <= 50 && mouseX - target.x >= 25) {
+     target.velocityX = -5;
+     target.velocityY = -5;
+   }
+
+   if(mouseY - target.y <= 50 && mouseY - target.y >= 25
+    && mouseX - target.x >= -50 && mouseX - target.x <= 25) {
+     target.velocityX = 5;
+     target.velocityY = -5;
+   }
+
+   if(mouseY - target.y >= -50 && mouseY - target.y <= 25
+    && mouseX - target.x <= 50 && mouseX - target.x >= 25) {
+     target.velocityX = -5;
+     target.velocityY = 5;
+   }
+
+   if(mouseY - target.y >= -50 && mouseY - target.y <= 25
+    && mouseX - target.x >= -50 && mouseX - target.x <= 25) {
+     target.velocityX = 5;
+     target.velocityY = 5;
+   }
+
+  drawSprites();
+}
